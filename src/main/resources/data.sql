7ef5ac2e0699632979451d50260f50adc0bcc684
niraj <nirajbnd@gmail.com>
2023-01-17 00:57:54 +0530

    spring cloud bus added for auto refresh

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.1', 'configA', 'val11');
+values('spring-config-jdbc-client', 'dev', '1.1', 'app.config.configA', 'val11');
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.1', 'configB', 'val12');
+values('spring-config-jdbc-client', 'dev', '1.1', 'app.config.configB', 'val12');
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.1', 'configC', 'val13');
+values('spring-config-jdbc-client', 'dev', '1.1', 'app.config.configC', 'val13');
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.0', 'configA', 'val21');
+values('spring-config-jdbc-client', 'dev', '1.0', 'app.config.configA', 'val21');
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.0', 'configB', 'val22');
+values('spring-config-jdbc-client', 'dev', '1.0', 'app.config.configB', 'val22');
 INSERT INTO CONFIG_PROPERTIES( APPLICATION, PROFILE, LABEL, PROP_KEY, VALUE) 
-values('spring-config-jdbc-client', 'dev', '1.0', 'configC', 'val23');
+values('spring-config-jdbc-client', 'dev', '1.0', 'app.config.configC', 'val23');
